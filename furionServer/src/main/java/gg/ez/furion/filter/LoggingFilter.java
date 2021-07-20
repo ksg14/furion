bb0dc9f0933f51435c1a744aff3eced56c7bad91
Karanjit Singh Gill <ksg207@live.com>
2021-07-20 14:11:51 +0530

    added Logging Filter to server

---
+++ furionServer/src/main/java/gg/ez/furion/filter/LoggingFilter.java
@@ -0,0 +1,47 @@
+package gg.ez.furion.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+
+    private final static Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public void doFilter (ServletRequest request,
+                          ServletResponse response,
+                          FilterChain filterChain)  {
+
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
+
+            logger.info("Logging Request  {} : {}",
+                    req.getMethod(),
+                    req.getRequestURI());
+
+            filterChain.doFilter (request,
+                    response);
+
+            logger.info("Logging Response {} : {} {}",
+                    req.getMethod(),
+                    req.getRequestURI(),
+                    res.getContentType());
+
+        } catch (Exception err) {
+            System.out.printf ("Logging Filter Error: %s", err.toString());
+        }
+    }
+}
